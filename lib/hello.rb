a20e25cb514ba7ad7ea88c0b47f9a69fbce90f14
meggs1 <megan.schertz@gmail.com>
2020-10-26 15:23:46 -0500

    commit tree 2c4d6a2edbd03c7850b48619e2cddd4406a30905

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,16 @@
-def hello_t
+def hello_t(array)
+    if block_given?
+        i = 0        # establishing a counter
 
+        while i < array.length
+            yield (array[i])        # the bracket method grabs the value of eacch element as we process the while loop
+            i = i + 1
+        end 
+
+        array
+    else 
+        puts "Hey! No block was given!"
+    end
 end
 
 # call your method here!
-
